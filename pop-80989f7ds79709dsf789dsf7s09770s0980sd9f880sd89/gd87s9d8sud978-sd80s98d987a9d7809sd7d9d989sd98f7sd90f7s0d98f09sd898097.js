3a294b3323aaf116b0c86a1b2285566bb6dfffd2
SKALEZ-A <94375178+SKALEZ-A@users.noreply.github.com>
2022-02-17 05:44:34 +0100

    kijhkpjpijpo;

---
+++ pop-80989f7ds79709dsf789dsf7s09770s0980sd9f880sd89/gd87s9d8sud978-sd80s98d987a9d7809sd7d9d989sd98f7sd90f7s0d98f09sd898097.js
@@ -0,0 +1,16 @@
+// const open = document.getElementById('open');
+// const model_container = document.getElementById('model_container');
+// const close = document.getElementById('close');
+
+// open.addEventListener('click', () => {
+//     model_container.classList.add('show');
+// });
+
+// close.addEventListener('click', () => {
+//     model_container.classList.remove('show');
+// });
+
+
+function togglePopup(){
+    document.getElementById("popup-1").classList.toggle("active");
+}
